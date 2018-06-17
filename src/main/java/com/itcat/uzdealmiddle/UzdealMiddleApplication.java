e68539b15b398b521a81de36907217ba43c0a3d2
Abin1994 <lilingbin1994@163.com>
2018-06-17 17:51:15 +0800

    Initial commit

---
+++ src/main/java/com/itcat/uzdealmiddle/UzdealMiddleApplication.java
@@ -0,0 +1,12 @@
+package com.itcat.uzdealmiddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzdealMiddleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzdealMiddleApplication.class, args);
+    }
+}

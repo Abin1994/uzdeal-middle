f8d657bc149acae652f76bc59875f58430b8d809
Abin1994 <lilingbin1994@163.com>
2018-06-17 20:52:57 +0800

    添加Fastjson

---
+++ src/main/java/com/itcat/uzdealmiddle/Controller/TestFastJson.java
@@ -0,0 +1,39 @@
+package com.itcat.uzdealmiddle.Controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.itcat.uzdealmiddle.domain.User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestFastJson {
+
+    //直接返回对象
+    @RequestMapping("testFastJson01")
+    public User testFastJson(){
+
+        User user = new User();
+        user.setName("tom");
+        user.setAge(15);
+        return user;
+    }
+
+    //返回一个对象的Json
+    @RequestMapping("testFastJson02")
+    public String testFastJson02(){
+
+        User user = new User();
+        user.setName("lily");
+        user.setAge(12);
+
+        String jsontoString = JSONObject.toJSONString(user);
+
+        System.out.println(user);
+        System.out.println(jsontoString);
+        return jsontoString;
+    }
+
+
+
+
+}

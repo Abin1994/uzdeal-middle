1cdfca394ef0ebd91586fb49da430af89f574e1d
Abin1994 <lilingbin1994@163.com>
2018-06-17 23:20:02 +0800

    添加lombok

--- src/main/java/com/itcat/uzdealmiddle/domain/User.java
+++ src/main/java/com/itcat/uzdealmiddle/domain/User.java
@@ -1,27 +1,20 @@
 package com.itcat.uzdealmiddle.domain;
 
+import lombok.Data;
+
+/*
+  *@Data
+  * @Data直接修饰POJO or beans， getter所有的变量，setter所有不为final的变量。
+  * 如果你不需要默认的生成方式，直接填写你需要的annotation的就可以了。
+  * 默认生成的所有的annotation都是public的，如果需要不同权限修饰符可以使用AccessLevel.NONE选项。当
+  * 然@Data也可以使用staticConstructor选项生成一个静态方法。
+     =@Setter+@Getter+@EqualsAndHashCode+@NoArgsConstructor
+  *
+ */
+@Data
 public class User {
 
     private String name;
     private int age;
 
-    public User() {
-
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
